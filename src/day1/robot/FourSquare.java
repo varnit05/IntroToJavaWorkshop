074d954c43c9d8cffab375ca81b24427e1568fef
Syu125 <league@WorthyMini-5.attlocal.net>
2017-02-21 11:58:52 -0800

    day 2 workshop
    i had fun making shapes with robots

--- src/day1/robot/FourSquare.java
+++ src/day1/robot/FourSquare.java
@@ -6,7 +6,7 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-	
+	Robot dave= new Robot("batman");
 
 	void go() {
 		// 4. Make the robot move as fast as possible
@@ -18,15 +18,24 @@ public class FourSquare {
 			// 7. Set the pen color to random
 	
 			// 1. Call the drawSquare() method
-	
+	drawSquare();
 			// 8. Turn the robot 90 degrees to the right
 
 	}
 
 	/* 3. Fill in the code to draw a square inside the method below. */
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
+		dave.setSpeed(10);
+			dave.setPenWidth(300);
+					dave.penDown();
+		for (int i = 0; i < 4; i++) {
+		dave.move(250);		dave.turn(90);
+	}
 
+		
+		
+		
+		
 		
 		
 	}
--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,13 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-
+Robot dave = new Robot ();
 	
-		
-		
+		dave.setRandomPenColor();
+		dave.penDown();
+		dave.setSpeed(9);
+		dave.move(398);
+		dave.turn(180);
 	}
 }
+
